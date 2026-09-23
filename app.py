e25f8ec15d42a726e47b0b258808687b15ec4218
Bhavneet <bhavneetk2828@gmail.com>
2026-09-23 07:39:27 +0530

    Clean up project configuration

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ from tools.final_answer import FinalAnswerTool
 
 from Gradio_UI import GradioUI
 
-# Below is an example of a tool that does nothing. Amaze us with your creativity !
-@tool
-def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
-    #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
-    Args:
-        arg1: the first argument
-        arg2: the second argument
-    """
-    return "What magic will you build ?"
-
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
     """A tool that fetches the current local time in a specified timezone.
@@ -55,7 +44,10 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[
+    get_current_time_in_timezone,
+    image_generation_tool,
+    final_answer],
     max_steps=6,
     verbosity_level=1,
     grammar=None,
